dfb2afcc18c52ab03958aadb38be387294fd50eb
SamirThaker <samir2009thaker@gmail.com>
2020-03-02 18:26:33 -0500

    completed code for list adapter for list view

--- app/src/main/java/com/example/myapplication_test1/Adapters/MainAdapter.java
+++ app/src/main/java/com/example/myapplication_test1/Adapters/MainAdapter.java
@@ -1,36 +1,57 @@
 package com.example.myapplication_test1.Adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.example.myapplication_test1.Modal.ListDetails;
+import com.example.myapplication_test1.R;
 
 import java.util.ArrayList;
 
 public class MainAdapter extends BaseAdapter {
     private Context context;
     ViewGroup.LayoutParams l;
+    public MainAdapter(Context context, ArrayList<ListDetails> headingArrayList) {
+        this.context = context;
+        HeadingArrayList = headingArrayList;
+    }
 
     private ArrayList<ListDetails> HeadingArrayList;
     @Override
     public int getCount() {
-        return 0;
+        return HeadingArrayList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return HeadingArrayList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).
+                    inflate(R.layout.mainlistlayout,parent, false);
+            convertView.setMinimumHeight(90);
+
+
+        }
+        TextView rowHeading = convertView.findViewById(R.id.txt1);
+
+
+        ListDetails items = (ListDetails) getItem(position);
+        if(items.getDialogHeader() !=null) {
+            rowHeading.setText(String.valueOf(items.getDialogHeader()));
+        }else {  }
+        return convertView;
     }
 }
